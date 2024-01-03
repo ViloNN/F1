42318d83401d12b090ac271acf5553bbb1ab44f0
szymon <szymonciruk5@gmail.com>
2024-01-03 22:40:12 +0100

    naprawa-linka

--- F1-site/src/App.vue
+++ F1-site/src/App.vue
@@ -47,8 +47,8 @@ onMounted(() => {
         </a>
         <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Formuła 1: Adrenalina i Szybkość</h1>
         <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Zapraszam do świata Formuły 1, gdzie innowacja spotyka się z prędkością. Przeżyj pasję i dynamikę najbardziej prestiżowych wyścigów na świecie!</p>
-        <div class="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-            <a class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+        <div class="text-gray-800 dark:text-white flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+            <a class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                 Obczaj więcej
                 <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </a>
@@ -103,7 +103,7 @@ onMounted(() => {
             </p>
           </div>
 
-          <a href="Cały weekend łączy w sobie intensywne przygotowania, strategię i emocjonującą rywalizację." title=""
+          <a href="https://www.formula1.com/en/latest/article.drivers-teams-cars-circuits-and-more-everything-you-need-to-know-about.7iQfL3Rivf1comzdqV5jwc.html" title=""
             class="bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
             role="button">
             Zobacz więcej informacji
